ce1548d49d1bcf0450ac88d42b203150be6f9bd5
konst100 <schwezkosnt@gmail.com>
2020-12-24 20:36:50 +0200

    foor commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload part 2 and best and zadolbal blin.
+
+          Edit <code>src/App.js</code> and save to reload part 2 and best and zadolbal blin.
         </p>
         <a
           className="App-link"
