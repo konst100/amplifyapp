7ae0a30f8e12cec1952e134430b0ead61dba81d8
konst100 <schwezkosnt@gmail.com>
2020-12-27 16:05:17 +0200

    raza

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload part 2 and best and zadolbal blin.
+          Edit <code>src/App.js</code> and save to reload part 2 and best and zadolbal blin dva raza.
 
 
           
